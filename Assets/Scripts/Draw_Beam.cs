bed268bcb8fd6f4e0f1ff04f21bc636d3b393443
Darcy V <87686851+KayNiner@users.noreply.github.com>
2023-01-12 11:51:31 +1100

    Added beam line rendered

---
+++ Assets/Scripts/Draw_Beam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Beam : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
+
+    public Transform origin;
+    public Transform Destination;
+
+    public float lineDrawSpeed = 6f;
+    //public float speed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, origin.position);
+        lineRenderer.SetWidth(7f, 7f);
+
+        dist = Vector3.Distance(origin.position, Destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter < dist)
+        {
+            counter += .1f / lineDrawSpeed;
+       
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = origin.Position;
+            Vector3 pointB = Destination.position;
+            //transfrom.translate(transform.parent.rotation * Vector3.up * speed);
+
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRenderer.SetPosition(1, pointAlongLine);
+        }
+    }
+}
