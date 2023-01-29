ccd37d0581f59107f523ae26f0901196f21d6094
H7CDR <thim1@student.holmesglen.edu.au>
2023-01-29 19:57:54 +1100

    Ring laser done with sound effect

--- Assets/Scripts/Draw_Beam.cs
+++ Assets/Scripts/Draw_Beam.cs
@@ -11,25 +11,38 @@ public class Draw_Beam : MonoBehaviour
     public Transform origin;
     public Transform Destination;
 
-    public float lineDrawSpeed = 6f;
+    public float lineDrawSpeed = 2f;
     public float speed = 1f;
+
+    [Header("Particle Trails")]
+    [SerializeField]
+    ParticleSystem particleTrail;
+    [SerializeField]
+    float distParticleToOrigin, distParticleToDestination;
+    
+    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, origin.position);
-        lineRenderer.SetWidth(0.3f, 0.3f);
-
+        //lineRenderer.SetPosition(0, origin.position);
+        lineRenderer.SetWidth(0f, 0f);
+        particleTrail.transform.position = origin.position;
         dist = Vector3.Distance(origin.position, Destination.position);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.SetPosition(0, origin.position);
-        if (counter < dist)
+        distParticleToOrigin = Vector3.Distance(particleTrail.transform.position, origin.position);
+        distParticleToDestination = Vector3.Distance(particleTrail.transform.position, Destination.position);
+        #region previous code
+        //lineRenderer.SetPosition(0, origin.position);
+        /*if (counter < dist)
         {
             counter += .1f / lineDrawSpeed;
        
@@ -41,7 +54,74 @@ public class Draw_Beam : MonoBehaviour
             Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
 
             lineRenderer.SetPosition(1, pointAlongLine);
+        }*/
+        //lineRenderer.SetPosition(1, Destination.position);
+        #endregion
+    }
+    public void drawLine()
+    {
+        Debug.Log("DrawlineActivate");
+        lineRenderer.SetPosition(0, origin.position) ;
+        //lineRenderer.SetWidth(0.3f, 0.3f);
+        particleOnMove();
+        counter = 0.1f;
+        dist = Vector3.Distance(origin.position, Destination.position);
+        if (counter < dist)
+        {
+            counter += .1f / lineDrawSpeed;
+
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = origin.position;
+            Vector3 pointB = Destination.position;
+
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRenderer.SetPosition(1, pointAlongLine);
         }
         lineRenderer.SetPosition(1, Destination.position);
     }
+    public void endLine()
+    {
+        Debug.Log("EndlineActivate");
+        lineRenderer.SetPosition(0, Destination.position);
+        //lineRenderer.SetWidth(0.3f, 0.3f);
+        
+        counter = 0.1f;
+
+        dist = Vector3.Distance(origin.position, Destination.position);
+        if (counter < dist)
+        {
+            counter += .1f / lineDrawSpeed;
+
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = origin.position;
+            Vector3 pointB = Destination.position;
+
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRenderer.SetPosition(1, pointAlongLine);
+        }
+        lineRenderer.SetPosition(1, Destination.position);
+    }
+
+    private void particleOnMove()
+    {
+       Debug.Log("Coroutine Start");
+       particleTrail.transform.position = origin.position;
+       lineRenderer.SetPosition(0, origin.position);
+       if (distParticleToOrigin < 0.2f)
+       {
+            particleTrail.transform.position = Destination.position;
+            
+       }
+       else if (distParticleToDestination<0.1f)
+       {
+            particleTrail.transform.position = origin.position;
+            
+       }
+       
+    }
+
 }
--- Assets/Scripts/PlayerControlled.cs
+++ Assets/Scripts/PlayerControlled.cs
@@ -45,10 +45,8 @@ public class PlayerControlled : MonoBehaviour
         //float lVertSpeed = Input.GetAxis("Oculus_GearVR_LThumbstickY") * Time.deltaTime;
         float rHoriSpeed = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal");
         float rVertSpeed = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical");
-        //Debug.Log(lHoriSpeed);
-        //Debug.Log(lVertSpeed);
         //Debug.Log("Y input: "+ rVertSpeed);
-        Debug.Log("X input: "+ rHoriSpeed);
+        //Debug.Log("X input: "+ rHoriSpeed);
         Debug.Log(hasXInput.ToString());
         uiOutPut.text = rVertSpeed.ToString();
 
@@ -68,27 +66,14 @@ public class PlayerControlled : MonoBehaviour
         {
             hasYInput = true;
         }
-            
-
-        //transform.localRotation = Quaternion.Euler(0, 0, (rVertSpeed-rHoriSpeed)*2000*Time.deltaTime);
-
-        //MOVEMENT
-        //Quaternion target = Quaternion.Euler(rVertSpeed, 0, -rHoriSpeed);
         Quaternion target = Quaternion.FromToRotation(Vector3.up, new Vector3(rHoriSpeed, rVertSpeed, 0));
         Quaternion target2 = Quaternion.Euler(0, 0, 180);
-        
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, new Vector3(rHoriSpeed,rVertSpeed, 0)), slerpSpeed);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, new Vector3(rHoriSpeed,rVertSpeed,0)), slerpSpeed);
-        //transform.rotation = Quaternion.FromToRotation(Vector3.up, new Vector3(lHoriSpeed, lVertSpeed, 0));
-        
-        //Dampen toward the target roation
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, slerpSpeed);
 
-        if(rVertSpeed >= -1f && rVertSpeed<-0.97)
+        if (rVertSpeed >= -1f && rVertSpeed < -0.97)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, target2, slerpSpeed);
         }
-        else if (hasXInput == false && rVertSpeed<0)
+        else if (hasXInput == false && rVertSpeed < 0)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, target2, slerpSpeed);
         }
@@ -96,5 +81,20 @@ public class PlayerControlled : MonoBehaviour
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, target, slerpSpeed);
         }
+        #region commented out codes
+        //transform.localRotation = Quaternion.Euler(0, 0, (rVertSpeed-rHoriSpeed)*2000*Time.deltaTime);
+
+        //MOVEMENT
+        //Quaternion target = Quaternion.Euler(rVertSpeed, 0, -rHoriSpeed);
+
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, new Vector3(rHoriSpeed,rVertSpeed, 0)), slerpSpeed);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, new Vector3(rHoriSpeed,rVertSpeed,0)), slerpSpeed);
+        //transform.rotation = Quaternion.FromToRotation(Vector3.up, new Vector3(lHoriSpeed, lVertSpeed, 0));
+
+        //Dampen toward the target roation
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, slerpSpeed);
+
+        #endregion
     }
 }
